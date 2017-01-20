b0b1bfea592e6468cdf005d0317bc8f541fb7c3c
siddhant <siddhant@siddhants-MacBook-Air.local>
2017-01-20 12:56:03 -0500

    final

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var mongojs = require("mongojs");
 
 
 // // Mongojs configuration
-var databaseUrl = "mongodb://heroku_jcjhxvx7:lqsfg22shtl09e8vbn1igrke2p@ds127878.mlab.com:27878/heroku_jcjhxvx7";
+var databaseUrl = "mongodb://heroku_jcjhxvx7:lqtl09e8vbn1igrke2p@ds127878.mlab.com:27878/heroku_jcjhxvx7";
 var collections = ["newsStories"];
 
 
@@ -27,80 +27,6 @@ db.on("error", function(error) {
 
 
 
-// // Configure Mongoose Database and Schema
-// // mongoose.connect('mongodb://heroku_jcjhxvx7:lqsfg22shtl09e8vbn1igrke2p@ds127878.mlab.com:27878/heroku_jcjhxvx7');
-
-// // var Schema = mongoose.Schema,
-// //     ObjectId = Schema.ObjectId;
- 
-// // var BlogPost = new Schema({
-// //     author    : ObjectId,
-// //     title     : String,
-// //     body      : String,
-// //     date      : Date
-// // });
-
-// // // retrieve my model 
-// // var BlogPost = mongoose.model('BlogPost');
- 
-// // // create a blog post 
-// // var post = new BlogPost();
- 
-// // // create a comment 
-// // post.comments.push({ title: 'My comment' });
- 
-// // post.save(function (err) {
-// //   if (!err) console.log('Success!');
-// // });
-// // Routes
-// // ================================================
-
-// app.get('/', function(req, res) {	
-// 	res.sendFile(path.join(__dirname, '/public/index.html'));
-// })
-
-// app.get('/headlines', function(req, res) {
-// 	db.newsStories.find(function(err, docs) {
-// 		res.json(docs);
-// 		console.log(docs);
-// 	});
-// })
-
-// // Scraping Headlines
-// // ================================================
-
-// // Run request to grab the HTML from google news
-/*******
-
-request("https://news.google.com/", function(error, response, html) {
-
-  // Load the HTML into cheerio
-  var $ = cheerio.load(html);
-
-  // Make an empty array for saving our scraped info
-  var result = [];
-
-  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-  $("span.titletext").each(function(i, element) {
-
-    // Cheerio's find method will "find" the first matching child element in a parent.
-    //    We start at the current element, then "find" its first child a-tag.
-    //    Then, we "find" the lone child img-tag in that a-tag.
-    //    Then, .attr grabs the imgs src value.
-    // So: <figure>  ->  <a>  ->  <img src="link">  ->  "link"  
-    var headline = $(element).text();//.find("a").attr("href")//.find("a")//.find("span").find("img")//.attr("src");//.attr("class").find("img").attr("src");
-
-    // Push the image's URL (saved to the imgLink var) into the result array
-    // result.push({ Link: headline });
-    // db.newsStories.save({Headline: headline})
-  });
-
-  // With each link scraped, log the result to the console
-  // console.log(result);
-  
-}); 
-
-******/
 
 module.exports = db;
 
